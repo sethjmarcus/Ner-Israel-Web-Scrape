891b0d1a4c5ab5c125949a4b1754cf64facfcce3
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2024-02-08 00:08:45 -0500

    saved with correct file name

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,15 +5,17 @@
 
 
 from scrapy.pipelines.files import FilesPipeline
-
-
+from itemadapter import ItemAdapter
+from scrapy import Request
 
 class TutorialPipeline(FilesPipeline):
-    #def process_item(self, item, spider):
-    #    return item
+    def get_media_requests(self, item, info):
+        return [Request(x, meta={'item': item}) for x in item.get(self.FILES_URLS_FIELD, [])]
+
 
     def file_path(self, request, response=None, info=None, *, item=None):
-        file_name: str = request.url.split("/")[-1]
+        item = request.meta.get('item')
+        file_name: str =  item['original_file_name'].replace(",", "_")
         print('\n\n', file_name, '\n\n')
         return file_name
 
